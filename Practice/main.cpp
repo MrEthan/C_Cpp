0d100c034ba35c4fc79349d3dfcfdc964fd4100f
MrEthan <ethan0ly@gmail.com>
2021-08-26 00:13:26 +0800

    set_new_handler例子

--- Practice/main.cpp
+++ Practice/main.cpp
@@ -4,7 +4,16 @@
 
 #include "my_smart_ptr.h"
 
+void outOfMem()
+{
+    std::cerr << "lack of memory" << std::endl;
+    std::abort();
+}
+
 int main(int argc, char **argv)
 {
+    std::set_new_handler(outOfMem);
     practice::smart_ptr();
+    int *pBigDataArray = new int[100000000000000000L];
+    return 0;
 }
